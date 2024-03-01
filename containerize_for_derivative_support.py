bfd1dc412f803d508a010813c4ff9415cb5ed716
DarthVader <144729384+fjeh98fjdhle@users.noreply.github.com>
2024-03-01 19:36:05 +0100

    Update containerize_for_derivative_support.py

--- containerize_for_derivative_support.py
+++ containerize_for_derivative_support.py
@@ -1,75 +1,49 @@
-def index_plus_finder(list):
-    initial_equation = list
-    #indexes of all the plus signs in the equation.
-    index_plus = []
-    index_count = -1 
-    for a in initial_equation: 
-        index_count += 1 
-        if a == "+":
-            index_plus.append(index_count + 1) # index is adjusted to "starting from" in a list so that when you start in a list you do not get the "+" as well.
+def true_index_list(index_list): 
+    #takes in the list and restores the true values for it. Given in this program where the indexes I work with are not noramlized to indexes in the list.
+    true_index_list = []
 
-    return index_plus
-
-
-
-# this returns if there is a paranthesis inbetween a given space for the x's in a list
-def check_for_paranthesis(index_1st, index_2nd, initial_equation): 
-    parenthesis = False
-    parenthesis_parts = []
-
-    #initial_values
-    parenthesis_parts.append(0)
-    parenthesis_parts.append(0)
-
-
-    for a in initial_equation[index_1st:index_2nd]: 
-        if a == "(": 
-            parenthesis_parts[0] += 1
-        elif a == ")":
-            parenthesis_parts[1] += 1
-
-    #the problem is what if there is an paranthesis in the function but it closes before the next "+" sign.
-    #To not write write for the case that one parenthesis is not closed we can generalize and say that if the number of parenthesises of both the start and the end are not equal, then it is true, that there is a paranthesis.     
-    if parenthesis_parts[0] == parenthesis_parts[1]:
-        parenthesis = False
-    if parenthesis_parts[0] != parenthesis_parts[1]:
-        parenthesis = True
-
-    return parenthesis      
-
-#marked: concept. when something is done you mark it.
-
-#This function stores all the values in a dictionary that have already been done so that when you have multiple "true's" in a sequence you do not append multiple times the same number to the right_intervalls list.
-def true_marked(count_x, count_finished_x, index_plus):
-    marked_x = []
-    #should store all the marked x
-
-    for a in index_plus[count_x:count_finished_x + 1]: 
-        marked_x.append(a)
+    for a in index_list: 
+        a = a -1
+        true_index_list.append(a)
     
-    return marked_x
+    return true_index_list
 
-    
-#this function goes through the index list and checks for parenthesis in the intervalls. if there are no paranthesis's in the intervall it will break the loop
-#In addition to that, it will store the start of the loop, meaning the start "x-value" and the end, the last number before the intervall gave false, so no paranthesis's. 
+
+
+#this function searches for the opposing close bracket of the opening bracket. That way I can make sure that, the +_sign that comes after it is not within the equation and that it can be treated by itself.
 def ttj(index_list, index_count, initial_equation): 
-    index_list = index_list[index_count:]
+    open_paranthesis = []
+    closed_paranthesis = []
     return_list = []
-    open_paran = []
-    closed_paran = []
+    
+    index_list = index_list[index_count:]
+    #zum Bearbeiten der originallen Funktion.
+    real_index_list = true_index_list(index_list)
 
+    #marked concept
+    #need to store for the index of the thing
+    marked_dictionary = {}
+    count = -1
+    for a in initial_equation[real_index_list[0]:]:
+        count += 1
+        marked_dictionary[count] = "undone" 
     count = -1 
-    for a in index_list:
-        count += 1 
-        if count +1 < len(index_list):
-            for w in initial_equation[index_list[0]:index_list[count +1]]:
+    for a in real_index_list:
+        count += 1
+        if count < len(index_list):
+            local_count = -1
+            for w in initial_equation[real_index_list[0]:real_index_list[count]]:
+                local_count += 1 
                 if w == "(":
-                    open_paran.append(index_list[count])
-                elif w == ")": 
-                    closed_paran.append 
-                if len(open_paran) == len(closed_paran):
-                    last = index_list[count+1]
-                    return_list.append(w)
-                    break
-
-    return last
+                    if marked_dictionary[local_count] == "undone":
+                        open_paranthesis.append(w)
+                        marked_dictionary[local_count] = "done"
+                if w == ")":
+                    if marked_dictionary[local_count] == "undone":
+                        closed_paranthesis.append(w)
+                        marked_dictionary[local_count] = "done"
+        if len(open_paranthesis) == len(closed_paranthesis): 
+            if len(open_paranthesis) != 0:
+                return_list.append(real_index_list[0] +1) #  the adjusted value for the list.
+                return_list.append(real_index_list[count]) # the adjusted stop value for the list.
+                break
